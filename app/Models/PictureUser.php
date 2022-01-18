c3f57a6c4ccb4fea26506936329f72bb33cd0bbf
fantomFM <sparkssss@bk.ru>
2022-01-18 18:25:57 +0800

    finishCommit

--- app/Models/PictureUser.php
+++ app/Models/PictureUser.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PictureUser extends Model
 {
-    // use HasFactory;
-
     /**
      * The database table associated with the model.
      *
